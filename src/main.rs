99148865497d968c7c8d5f7956f1273b778acdf3
haithcock <dakota.haith1997@.com>
2025-06-12 00:37:05 -0400

    messed up rust code

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-/*fn greet(hello: &str) {
-    println!("Hello, {}!", hello)
-}
 
-fn main() {
-    greet("How are you today?");
-}*/
+
+fn print_age(new_age: i32) {
+println!("{}", print_age);
+}
 fn main(){
+    println!("");
+    print_age(69);
 let name = "Kim";
 let age : i32 = 69;
 let height: f64 = 1.85;
 let likes_rust: bool= true;
 println! ("Hello my name is {} and I am {} years old and {} feet tall. Do I like rust? {}", name, age, height, likes_rust);
+println!("Lola the dog is {} years old", age + 1);
+
 }
+
