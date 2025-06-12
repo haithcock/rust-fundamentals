6483772d6fae21387be54d714f42d46556618444
haithcock <dakota.haith1997@.com>
2025-06-11 23:55:19 -0400

    learning rust syntax

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+/*fn greet(hello: &str) {
+    println!("Hello, {}!", hello)
 }
+
+fn main() {
+    greet("How are you today?");
+}*/
+fn main(){
+let name = "Kim";
+let age : i32 = 69;
+let height: f64 = 1.85;
+println! ("Hello my name is {} and I am {} years old and {} feet tall", name, age, height);
+
+}
+/*
+Modify your existing program to:
+
+    Add an explicit type to age (use i32).
+
+    Create a new variable height with type f64 (e.g., 1.85).
+
+    Print both age and height in one println!.
+*/
