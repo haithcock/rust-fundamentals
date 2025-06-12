a34e8e143bef7ee96f82b25d8eead96710a49a0b
haithcock <dakota.haith1997@.com>
2025-06-12 00:05:31 -0400

    updoots

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,6 @@ fn main(){
 let name = "Kim";
 let age : i32 = 69;
 let height: f64 = 1.85;
-println! ("Hello my name is {} and I am {} years old and {} feet tall", name, age, height);
-
+let likes_rust: bool= true;
+println! ("Hello my name is {} and I am {} years old and {} feet tall. Do I like rust? {}", name, age, height, likes_rust);
 }
-/*
-Modify your existing program to:
-
-    Add an explicit type to age (use i32).
-
-    Create a new variable height with type f64 (e.g., 1.85).
-
-    Print both age and height in one println!.
-*/
